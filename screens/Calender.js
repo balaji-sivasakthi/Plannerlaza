26d2f79abcf35de718603f425004e7400526928b
balaji-sivasakthi <balajisivasakthi2020@gmail.com>
2022-04-26 18:12:51 +0530

    calender

--- screens/Calender.js
+++ screens/Calender.js
@@ -1,23 +1,67 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
-import { colors } from "../theme";
-import { Calendar } from "react-native-calendars";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native"
+import React from "react"
+import { colors } from "../theme"
+import { Agenda, Calendar } from "react-native-calendars"
+import { useState } from "react"
 
 const Calender = () => {
+  const [events, setevents] = useState({})
+  const timeToString = (time) => {
+    const date = new Date(time)
+    return date.toISOString().split("T")[0]
+  }
+  const loadItems = (day) => {
+    const items = events || {}
+
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000
+        const strTime = timeToString(time)
+
+        if (!items[strTime]) {
+          items[strTime] = []
+
+          const numItems = Math.floor(Math.random() * 3 + 1)
+          for (let j = 0; j < numItems; j++) {
+            items[strTime].push({
+              name: "Item for " + strTime + " #" + j,
+              height: Math.max(50, Math.floor(Math.random() * 150)),
+              day: strTime,
+            })
+          }
+        }
+      }
+
+      const newItems = {}
+      Object.keys(items).forEach((key) => {
+        newItems[key] = items[key]
+      })
+      setevents({
+        items: newItems,
+      })
+    }, 1000)
+  }
   return (
     <View style={styles.container}>
-    
+      <Agenda
+        items={loadItems}
+        loadItemsForMonth={loadItems} // Function
+        renderItem={(item) => {
+          return (
+            <View>
+              <Text>{item.title}</Text>
+            </View>
+          )
+        }}
+      />
     </View>
-  );
-};
+  )
+}
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: colors.backgroundColor,
-    width: "100%",
-    height: "100%",
-    justifyContent: "center",
-    alignItems: "center",
   },
-});
+})
 
-export default Calender;
+export default Calender
